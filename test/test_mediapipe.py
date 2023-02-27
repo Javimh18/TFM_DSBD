85830d7e53c564e8e45519bbe71e0a6a605b4c65
Javimh18 <javier.munnoz.haro@gmail.com>
2023-02-27 14:39:59 +0100

    mediapipe transforms now working

---
+++ test/test_mediapipe.py
@@ -0,0 +1,107 @@
+import cv2
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+import time
+import mediapipe as mp
+
+mp_holistic = mp.solutions.holistic # Holistic model
+mp_drawing = mp.solutions.drawing_utils # Drawing utilities
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False                  # Image is no longer writeable
+    results = model.process(image)                 # Make prediction
+    image.flags.writeable = True                   # Image is now writeable 
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+    return image, results
+
+def draw_styled_landmarks(image, results):
+    # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=1), 
+                             mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=.5)
+                             ) 
+    # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                             mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=1), 
+                             mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=.5)
+                             ) 
+    # Draw right hand connections  
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                             mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=1), 
+                             mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=.5)
+                             ) 
+
+def test_mediapipe_live():
+    cap = cv2.VideoCapture(0)
+    # Set mediapipe model 
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+        while cap.isOpened():
+
+            # Read feed
+            ret, frame = cap.read()
+
+            # Make detections
+            image, results = mediapipe_detection(frame, holistic)
+            print(results)
+            
+            # Draw landmarks
+            draw_styled_landmarks(image, results)
+
+            # Show to screen
+            cv2.imshow('OpenCV Feed', image)
+
+            # Break gracefully
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+        cap.release()
+        cv2.destroyAllWindows()
+
+def test_mediapipe_video():
+    video_path = '../data/videos/47614.mp4'
+    cap = cv2.VideoCapture(video_path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    fourCC = cv2.VideoWriter_fourcc('M','J','P','G')
+    out = cv2.VideoWriter('mp_test.mp4', fourCC, fps, (frame_width,frame_height))
+
+
+    counter = 0
+    # declaration of the holistic model in media pipe
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+        # Loop until the end of the video
+        while (cap.isOpened()):
+            
+            # Capture frame by frame
+            ret, frame = cap.read()
+
+            if ret:
+                # Make detections
+                results = holistic.process(frame)
+                if results is None:
+                    print("Something is wrong w/ mediapipe... Exiting.")
+                    cap.release()
+                    out.release()
+                    return
+
+                # Draw landmarks
+                draw_styled_landmarks(frame, results)
+                
+                out.write(frame)
+                counter += 1
+            else:
+                break
+
+        cap.release()
+        out.release()
+
+    
+if __name__ == '__main__':
+    # This is for testing with a live video
+    # test_mediapipe_live()
+    # This is for testing with saved videos
+    test_mediapipe_video()
