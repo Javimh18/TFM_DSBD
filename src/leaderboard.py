0ccda48d114791ba3a7e10b36ebb991811f5cd93
Javimh18 <javier.munnozharo@estudiante.uam.es>
2023-04-29 15:05:13 +0200

    Added subset size to leaderboard

--- src/leaderboard.py
+++ src/leaderboard.py
@@ -6,10 +6,10 @@ class Leaderboard:
         self.path_to_lb = path_to_lb
         self.lb = pd.read_csv(path_to_lb)
 
-    def update_leaderboard(self, history, test_results, model_name, model_type):
+    def update_leaderboard(self, history, test_results, model_name, model_type, subset_size):
         # check if the type of the model exists. In that case update that entry on
         # the leader board. If not, create new entry to the dataframe
-        if (True in self.lb['model_type'].isin(model_type)) and (True in self.lb['model_name'].isin(model_name)):
+        if (True in self.lb['model_type'].isin(model_type)) and (True in self.lb['model_name'].isin(model_name)) and (True in self.lb['subset_size'].isin(subset_size)):
             row = self.lb.loc[(self.lb['model_type'] == model_type) and (self.lb['model_name'] == model_name)]
             val_acc_epoch = np.argmax(history.history['val_accuracy']) 
             val_acc_max = history.history['val_accuracy'][val_acc_epoch]
@@ -34,6 +34,7 @@ class Leaderboard:
             test_loss_max = test_results[0]
             epoch = val_acc_epoch + 1
             path_to_model =  f'src/{model_type}/bestmodels/best_{model_name}_{epoch}_{val_acc_max}_model.h5'
+            subset_size = subset_size
 
             new_row = {
                 'model_type': model_type,
@@ -45,7 +46,8 @@ class Leaderboard:
                 'val_loss': val_loss_max,
                 'test_loss': test_loss_max,
                 'epoch': epoch,
-                'path_to_model': path_to_model
+                'path_to_model': path_to_model,
+                'subset_size': subset_size
             }
 
             # we add the new row to the dataframe and sort the dataframe based on the val_acc
@@ -55,4 +57,10 @@ class Leaderboard:
         # we save the updated version of the leaderboard
         self.lb.to_csv(self.path_to_lb, header=True, index=False, mode='w+')
 
+
+    def create_leaderboard(path_to_save='data/leaderboard'):
+        df = pd.DataFrame(columns=['model_type', 'model_name', 'train_acc', 'val_acc', 'test_acc', 'train_loss', 'val_loss', 'test_loss', 'epoch', "path_to_model", "subset_size"])
+        df.to_csv(path_to_save)
+
+    
             
